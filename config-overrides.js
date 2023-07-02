b92e7d55eda7c31093f66b810700ea2527b4bf9e
Nischal Dhungana <34458006+mrhxszo@users.noreply.github.com>
2023-07-02 17:39:10 +0200

    Updates

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = function override(config) {
-  config.resolve.fallback = {
-    stream: require.resolve('stream-browserify'),
-  };
-  return config;
-};
--- typings.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.png";
-declare module "*.jpg";
-declare module '*.pdf' {
-    const content: any;
-    export default content;
-  }
- 
-  
